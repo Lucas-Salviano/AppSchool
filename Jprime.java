fd2a7599ab1f75e7d1f5676a55d53c16a226638c
Yuri Severo Pinto Costa <127903285+Yuri-Severo@users.noreply.github.com>
2023-11-25 22:08:52 -0300

    Update Jprime.java

--- Jprime.java
+++ Jprime.java
@@ -1,36 +1,32 @@
 package com.mycompany.jprime;
 
 import java.util.List;
-
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Jprime {
+    
 
     public static void main(String[] args) {
+        List<Escola> listaEscolas;
+
+        Scanner input = new Scanner(System.in);
+
+        listaEscolas = new ArrayList<>();
+
+        System.out.println("GERENCIADOR DE ESCOLAS");
+        System.out.println("Por favor, cadastre a primeira Escola:");
+        System.out.println("Qual o nome da escola?");
+        String nomeEscola1 = input.next();
+
+        Escola Escola1 = new Escola(nomeEscola1);
+        listaEscolas.add(Escola1);
+        Escola1.criarEscola();
+        Escola1.ExibirInfo();
+
         
         
-        Curso curso1 = new Curso("Ciência da Computação", 1);
-        Curso curso2 = new Curso("Engenharia Elétrica", 2);
+
         
-        Alunos aluno = new Alunos("adler",33454,3,16,06,3433);
-        
-        Alunos aluno2 = new Alunos("gir",43534,6,17,12,34535);
-        
-        curso1.adicionarAluno(aluno);
-        curso2.adicionarAluno(aluno2);
-        curso1.adicionarAluno(aluno2);
-        
-        //teste exibindo alunos e informações deles, isto deve ser melhorado
-        
-        List<Alunos> alunosCurso1 = curso1.getAlunos();
-        for (Alunos alunos : alunosCurso1) {
-            System.out.println("Aluno do curso 1: " + alunos.getNomeA());
-            alunos.exibirAlunos();
-        }
-        
-         List<Alunos> alunosCurso2 = curso2.getAlunos();
-        for (Alunos alunos : alunosCurso2) {
-            System.out.println("Aluno do curso 2: " + alunos.getNomeA());
-            alunos.exibirAlunos();
-        }
     }
 }
